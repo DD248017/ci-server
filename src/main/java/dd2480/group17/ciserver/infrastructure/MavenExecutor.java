c5ea866bca1c0c779de858db1ab2d392a08a658c
20GR02 <riccogiacomo02@gmail.com>
2025-02-09 18:52:51 +0100

    Feat: adds MavenCompileRunner and MavenExecutor

--- src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
+++ src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
@@ -1,4 +1,31 @@
 package dd2480.group17.ciserver.infrastructure;
 
+import dd2480.group17.ciserver.utils.MavenCompileRunner;
+import dd2480.group17.ciserver.utils.MavenTestRunner;
+
 public class MavenExecutor {
+
+    // Metodo per eseguire la compilazione Maven
+    public void runCompile() {
+        System.out.println("Starting Maven compilation...");
+        MavenCompileRunner.runMavenCompile();
+    }
+
+    // Metodo per eseguire i test Maven
+    public void runTest() {
+        System.out.println("Starting Maven tests...");
+        MavenTestRunner.runMavenTest();
+    }
+
+    // Metodo per eseguire entrambe le operazioni (compilazione e test)
+    public void runAll() {
+        runCompile();
+        runTest();
+    }
+
+    public static void main(String[] args) {
+        // Eseguiamo tutto: compilazione e test
+        MavenExecutor executor = new MavenExecutor();
+        executor.runAll();
+    }
 }
