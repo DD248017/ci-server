a059181eafdc364b89fc9b39dd1f8de151f6034f
maxaik <maxolof1@hotmail.com>
2025-02-09 18:53:19 +0100

    refactor: New handler system
    - Map routes to specific handler
    - Using WebhookDTO instead of json
    - Added health, history and webhook route
    
    Related to #25

--- src/main/java/dd2480/group17/ciserver/service/WebhookService.java
+++ src/main/java/dd2480/group17/ciserver/service/WebhookService.java
@@ -1,6 +1,6 @@
 package dd2480.group17.ciserver.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import dd2480.group17.ciserver.infrastructure.dto.WebhookDTO;
 
 public class WebhookService {
 
@@ -10,50 +10,34 @@ public class WebhookService {
     private static final NotificationService notificationService = new NotificationService();
     private static final HistoryService historyService = new HistoryService();
 
-    public void processWebhookEvent(JsonNode jsonPayload) {
+    // TODO javadocs
+    public void processWebhookEvent(WebhookDTO webhookDTO) {
 
-        // Test this if you want to clone a repo
-        /*
-         * boolean cloneSuccess =
-         * gitService.fetchRepository("https://github.com/DD248017/ci-server", "test",
-         * "./ci-server-test");
-         */
-
-        if (isGitHubPushEvent(jsonPayload)) {
-            handlePushEvent(jsonPayload);
-        } else {
-            System.out.println("Not a GitHub push event.");
-
-        }
-
+        // Error handling here or check if it is pushevent
+        handlePushEvent(webhookDTO);
     }
 
-    private boolean isGitHubPushEvent(JsonNode jsonPayload) {
-        JsonNode refNode = jsonPayload.path("ref");
-        return !refNode.isMissingNode() && refNode.asText().startsWith("refs/heads/");
-    }
-
-    // TODO fix right function parameters
-    private void handlePushEvent(JsonNode jsonPayload) {
-
-        String repoUrl = jsonPayload.path("repository").path("clone_url").asText();
-        String branch = jsonPayload.path("ref").asText().replace("refs/heads/", "");
+    // TODO get this work with other functions and javadocs
+    private void handlePushEvent(WebhookDTO webhookDTO) {
+        String repoUrl = webhookDTO.repository().htmlUrl();
+        String branch = webhookDTO.branch();
+        String commitHash = webhookDTO.after();
 
         try {
             boolean cloneSuccess = gitService.fetchRepository(repoUrl, branch, "./newRepo");
             if (cloneSuccess) {
-                boolean compileSuccess = compileService.compileCode("somepath");
-                boolean testSuccess = testService.executeTests("somepath");
+                boolean compileSuccess = compileService.compileCode("./newRepo");
+                boolean testSuccess = testService.executeTests("./newRepo");
 
                 // Notify based on the result of compile and test
                 if (compileSuccess && testSuccess) {
-                    notificationService.notifySuccess("some hash");
+                    notificationService.notifySuccess(commitHash);
                 } else {
-                    notificationService.notifyFailure("some hash");
+                    notificationService.notifyFailure(commitHash);
                 }
 
                 // Log the build history
-                historyService.logBuild(repoUrl, branch, "some hash", compileSuccess, testSuccess);
+                historyService.logBuild(repoUrl, branch, commitHash, compileSuccess, testSuccess);
             } else {
                 // Notify failure to clone repository
                 notificationService.notifyFailure("Failed to clone repository.");
