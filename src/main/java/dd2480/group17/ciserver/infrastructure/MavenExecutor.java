cdb4a088e24ea0c9bae7958c1abd9468c4db2238
20GR02 <riccogiacomo02@gmail.com>
2025-02-10 09:05:54 +0100

    Feat: adds MavenCompileRunner and MavenExecutor  (#27)
    * Feat: adds MavenCompileRunner and MavenExecutor
    
    * fix: errors in the MavenExecutor file
    related to #20
    
    * refactor(MavenExecutor):
    
    - Combined mvn test and compile to one function
    - added repo path in mvn util function
    - Return true or false depending on test / compile results
    
    Related to #20
    
    ---------
    
    Co-authored-by: maxaik <maxolof1@hotmail.com>

--- src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
+++ src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
@@ -1,4 +1,14 @@
 package dd2480.group17.ciserver.infrastructure;
 
+import dd2480.group17.ciserver.utils.MavenRunner;
+
 public class MavenExecutor {
+
+    public boolean runCompile(String repoPath) {
+        return MavenRunner.runMaven("compile", repoPath);
+    }
+
+    public boolean runTest(String repoPath) {
+        return MavenRunner.runMaven("test", repoPath);
+    }
 }
