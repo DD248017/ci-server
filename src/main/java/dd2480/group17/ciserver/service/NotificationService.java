f689e2e4054d3401f7cff5c4db1553000eeceb9c
Simon Li <simoli@kth.se>
2025-02-10 14:05:19 +0100

    feat: Add service to notify GitHub about build success or failure.
    relates to #15 and #16

--- src/main/java/dd2480/group17/ciserver/service/NotificationService.java
+++ src/main/java/dd2480/group17/ciserver/service/NotificationService.java
@@ -2,17 +2,28 @@ package dd2480.group17.ciserver.service;
 
 import dd2480.group17.ciserver.infrastructure.StatusSender;
 
+/**
+ * The {@code NotificationService} class is responsible for notifying GitHub
+ * about the success or failure of a build by updating the commit status.
+ */
 public class NotificationService {
     private static final StatusSender statusSender = new StatusSender();
 
-    // Maybe to this in one func
-
-    // TODO call status sender
+    /**
+     * Notifies GitHub that the build for the given commit was successful.
+     *
+     * @param commitHash The SHA hash of the commit to update the status for.
+     */
     public void notifySuccess(String commitHash) {
+        statusSender.sendStatus(commitHash, true);
     }
 
-    // TODO call status sender
+    /**
+     * Notifies GitHub that the build for the given commit has failed.
+     *
+     * @param commitHash The SHA hash of the commit to update the status for.
+     */
     public void notifyFailure(String commitHash) {
+        statusSender.sendStatus(commitHash, false);
     }
-
 }
