d151f24ba8624863259550cb98dd0a65c462f35f
maxaik <maxolof1@hotmail.com>
2025-02-10 13:31:13 +0100

    feat(HistoryHandler): Added html page
    - Implemeted function to display logDTO in html format
    - Get request on /history will recive html content
    
    Related to #28

--- src/main/java/dd2480/group17/ciserver/infrastructure/HistoryHandler.java
+++ src/main/java/dd2480/group17/ciserver/infrastructure/HistoryHandler.java
@@ -1,6 +1,7 @@
 package dd2480.group17.ciserver.infrastructure;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,15 +9,60 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import dd2480.group17.ciserver.infrastructure.dto.LogDTO;
+import dd2480.group17.ciserver.service.HistoryService;
+
 public class HistoryHandler extends AbstractHandler {
+
+    private static final HistoryService historyService = new HistoryService();
+
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+
+        String compileLogPath = "./src/main/resources/dd2480/group17/ciserver/logs/compile";
+        String testLogPath = "./src/main/resources/dd2480/group17/ciserver/logs/test";
+
+        List<LogDTO> CompilelogEntries = historyService.processAllLogsInDirectory(compileLogPath);
+        List<LogDTO> TestlogEntries = historyService.processAllLogsInDirectory(testLogPath);
 
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-        response.getWriter().println("<h1>TODO - Show history here!</h1>");
+        String pageContent = displayCompileLogs(CompilelogEntries) + displayTestLogs(TestlogEntries);
+
+        response.getWriter().println(pageContent);
     }
+
+    private String displayCompileLogs(List<LogDTO> logEntries) {
+        String html = "<h1>Compile log history</h1>";
+        html += "<table border='1'><tr><th>Commit Id</th><th>Filename</th><th>Content</th></tr>";
+
+        for (LogDTO entry : logEntries) {
+            html += ("<tr>" + "<td>" + entry.getCommitId() + "</td>"
+                    + "<td>" + entry.getFilename() + "</td>"
+                    + "<td>" + entry.getErrorOutput() + "</td>"
+                    + "</tr>");
+        }
+
+        html += "</table>";
+        return html;
+    }
+
+    private String displayTestLogs(List<LogDTO> logEntries) {
+        String html = "<h1>Test log history</h1>";
+        html += "<table border='1'><tr><th>Commit Id</th><th>Filename</th><th>Content</th></tr>";
+
+        for (LogDTO entry : logEntries) {
+            html += ("<tr>" + "<td>" + entry.getCommitId() + "</td>"
+                    + "<td>" + entry.getFilename() + "</td>"
+                    + "<td>" + entry.getErrorOutput() + "</td>"
+                    + "</tr>");
+        }
+
+        html += "</table>";
+        return html;
+    }
+
 }
