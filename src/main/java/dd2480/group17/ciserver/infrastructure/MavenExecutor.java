7fef0baca16f1c5cc53505ee71280619bef34278
20GR02 <riccogiacomo02@gmail.com>
2025-02-11 15:52:55 +0100

    doc: add Javadoc for MavenExecutor file functions related to #54

--- src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
+++ src/main/java/dd2480/group17/ciserver/infrastructure/MavenExecutor.java
@@ -5,14 +5,32 @@ import dd2480.group17.ciserver.infrastructure.dto.TestDTO;
 import dd2480.group17.ciserver.utils.MavenCompileRunner;
 import dd2480.group17.ciserver.utils.MavenTestRunner;
 
+/**
+ * The {@code MavenExecutor} class provides methods to execute Maven commands
+ * for compiling and testing a project.
+ */
 public class MavenExecutor {
 
+    /**
+     * Runs the Maven compile command on the specified repository path.
+     *
+     * @param repoPath the path to the repository where the Maven compile command
+     *                 should be executed
+     * @return a {@link CompileDTO} object containing the result of the compilation
+     *         process
+     */
     public CompileDTO runCompile(String repoPath) {
         return MavenCompileRunner.runMavenCompile("compile", repoPath);
     }
 
+    /**
+     * Runs the Maven test command on the specified repository path.
+     *
+     * @param repoPath the path to the repository where the Maven test command
+     *                 should be executed
+     * @return a {@link TestDTO} object containing the result of the test execution
+     */
     public TestDTO runTest(String repoPath) {
         return MavenTestRunner.runMavenTest("test", repoPath);
     }
-
 }
