4284d8f4b854243c3264060257a07c364d2d2988
kohei kuramoto <kuramoto@kth.se>
2025-02-11 14:10:59 +0100

    docs: Add comprehensive Javadoc comments to CompileService.java

--- src/main/java/dd2480/group17/ciserver/service/GitService.java
+++ src/main/java/dd2480/group17/ciserver/service/GitService.java
@@ -6,7 +6,16 @@ public class GitService {
 
     private static final GitHandler gitHandler = new GitHandler();
 
-    // TODO call githubhandler to get the repository
+    /**
+     * Fetches the repository by cloning it from the specified URL, branch and
+     * target directory
+     * 
+     * @param repoUrl   the url of the repository
+     * @param branch    the branch to clone
+     * @param targetDir the directory where the repository should be cloned
+     * @return return true if the repository was successfully cloned, false
+     *         otherwise
+     */
     public boolean fetchRepository(String repoUrl, String branch, String targetDir) {
 
         boolean successful = gitHandler.cloneRepository(repoUrl, branch, targetDir);
