26ec909066dcafb362f042cf852935df797dd629
maxaik <maxolof1@hotmail.com>
2025-02-12 16:14:43 +0100

    80 service test for compile and test (#81)
    * test(testBuilds): cloned 4 test repos
    
    - Use this repos to test services
    
    Related to #80
    
    * test(CompileService): Added test for compile
    
    - test mvn compile
    
    Related to #80
    
    * test(TestServices): Added tests for test service
    
    - test mvn test
    
    Related to #80
    
    * refactor(testBuilds): Uploaded testBuilds as a zip
    
    - Using zip folder instead of add all test builds
    
    Related to #80

--- src/test/java/dd2480/group17/ciserver/service/CompileServiceTest.java
+++ src/test/java/dd2480/group17/ciserver/service/CompileServiceTest.java
@@ -1,4 +1,35 @@
 package dd2480.group17.ciserver.service;
 
-public class CompileServiceTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class CompileServiceTest {
+
+    private static final CompileService compileService = new CompileService();
+
+    @Test
+    void testCompileSuccess() {
+
+        String repoPath = "./testBuilds/compileSuccess";
+        String commitId = "some test id";
+
+        boolean result = compileService.compileCode(repoPath, commitId);
+
+        assertTrue(result, "Compilation should be successful");
+
+    }
+
+    @Test
+    void testCompileFail() {
+
+        String repoPath = "./testBuilds/compileFail";
+        String commitId = "some test id";
+
+        boolean result = compileService.compileCode(repoPath, commitId);
+
+        assertFalse(result, "Compilation should be fail");
+
+    }
+
 }
--- src/test/java/dd2480/group17/ciserver/service/TestServiceTest.java
+++ src/test/java/dd2480/group17/ciserver/service/TestServiceTest.java
@@ -1,4 +1,35 @@
 package dd2480.group17.ciserver.service;
 
-public class TestServiceTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class TestServiceTest {
+
+    private static final TestService testService = new TestService();
+
+    @Test
+    void testExecuteTestsSuccess() {
+
+        String repoPath = "./testBuilds/testSuccess";
+        String commitId = "some test id";
+
+        boolean result = testService.executeTests(repoPath, commitId);
+
+        assertTrue(result, "Compilation should be successful");
+
+    }
+
+    @Test
+    void testExecuteTestsFail() {
+
+        String repoPath = "./testBuilds/testFail";
+        String commitId = "some test id";
+
+        boolean result = testService.executeTests(repoPath, commitId);
+
+        assertFalse(result, "Compilation should be fail");
+
+    }
+
 }
